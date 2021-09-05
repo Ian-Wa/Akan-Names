62555cbf7a633868191d3eb31cd6bee6ec8fb92f
Ian <ian.wanjira@student.moringaschool.com>
2021-09-05 20:50:12 +0300

    add a validate function to the main.js file

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,20 @@
-let daysOfTheWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-let namesOfMale = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
-let namesOfFemale =["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama",]
+let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+let Male = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+let Female =["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama",];
+
+function validate () {
+    const year = parseInt(document.getElementById('year').value);
+    const day = parseInt(document.getElementById('day').value);
+    const month = parseInt(document.getElementById('month').value);
+
+    if( year <= 0 || day < 0 || day >= 31 || month <= 0 || month > 12){
+        alert("Please enter valid numbers");
+    }
+}
+
+function checkName () {
+    validate ();
+    const dayValue = 
+
+}
+
